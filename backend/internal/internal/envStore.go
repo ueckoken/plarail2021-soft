4961c1d85adea66f38792e24490a79f056e54ad2
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2021-10-30 16:17:53 +0900

    implement metrics handler in internal server

--- backend/internal/internal/envStore.go
+++ backend/internal/internal/envStore.go
@@ -29,6 +29,7 @@ type Env struct {
 	ExternalSideServer struct {
 		Port Port `envconfig:"default=54321"`
 		//SslCertPath string
+		MetricsPort Port `envconfig:"default=8080"`
 	}
 	NodeConnection struct {
 		Timeout time.Duration `envconfig:"default=1s"`
