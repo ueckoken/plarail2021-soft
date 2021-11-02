538a6edfe00b7a6ded759c43148b0faa41836cb1
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2021-11-03 01:18:42 +0900

    init pipenv

--- backend/positioning/visualize.py
+++ backend/positioning/visualize.py
@@ -12,11 +12,12 @@ G = nx.DiGraph()
 G.add_nodes_from(list(map(lambda x: x["name"], y)))
 s = []
 for i, n in enumerate(y):
-    if n["nexts"]==None:
+    if n["nexts"] is None:
         continue
     for j, m in enumerate(n["nexts"]):
-        s.append((n["name"],m))
+        s.append((n["name"], m))
 G.add_edges_from(s)
 nx.spring_layout(G)
-nx.draw(G,pos=nx.nx_pydot.graphviz_layout(G),node_color="w",alpha=0.6, node_size=3, with_labels=True)
+nx.draw(G, pos=nx.nx_pydot.graphviz_layout(G), node_color="w", alpha=0.6,
+        node_size=3, with_labels=True)
 plt.show()
