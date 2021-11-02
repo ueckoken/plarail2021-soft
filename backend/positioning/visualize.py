367db94656a796ba1f309626eb44c46eb4c2a729
gotti <18141824+gotti@users.noreply.github.com>
2021-11-03 01:06:45 +0900

    みやすい

--- backend/positioning/visualize.py
+++ backend/positioning/visualize.py
@@ -12,14 +12,11 @@ G = nx.DiGraph()
 G.add_nodes_from(list(map(lambda x: x["name"], y)))
 s = []
 for i, n in enumerate(y):
-    print("name")
     if n["nexts"]==None:
         continue
     for j, m in enumerate(n["nexts"]):
-        print(m)
         s.append((n["name"],m))
 G.add_edges_from(s)
-plt.figure(figsize=(15,15))
 nx.spring_layout(G)
-nx.draw(G,node_color="w",alpha=0.6, node_size=3, with_labels=True)
+nx.draw(G,pos=nx.nx_pydot.graphviz_layout(G),node_color="w",alpha=0.6, node_size=3, with_labels=True)
 plt.show()
