9be7c0bd549071b7766930039859cc31bee98f1e
kento <asikaga613-kento@yahoo.co.jp>
2021-11-14 00:27:46 +0900

    fix endpoint

--- frontend/videoCast/webpack.config.js
+++ frontend/videoCast/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      SW_WSURL: '"webrtc.chofufes2021.gotti.dev"',
+      SW_WSURL: '"wss://webrtc.chofufes2021.gotti.dev/"',
       SKYWAY_APIKEY: JSON.stringify(process.env.SKYWAY_APIKEY),
       SKYWAY_DEBUG_LEVEL: "2",
       AYAME_SIGNALING_KEY: JSON.stringify(process.env.AYAME_SIGNALING_KEY),
