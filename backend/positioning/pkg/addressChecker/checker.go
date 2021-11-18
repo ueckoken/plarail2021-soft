50dbd504b994469ddc20bc35fcb7e2ba1d9381ad
gotti <18141824+gotti@users.noreply.github.com>
2021-11-18 15:19:56 +0900

    restrict

--- backend/positioning/pkg/addressChecker/checker.go
+++ backend/positioning/pkg/addressChecker/checker.go
@@ -12,43 +12,43 @@ import (
 var AllowListYaml []byte
 
 type AddressChecker struct {
-  allowlist []string
+	allowlist []string
 }
 
-func (a AddressChecker)CheckIfOk(addr string) bool{
-  for _, d := range a.allowlist{
-    _, ipnet, err := net.ParseCIDR(d)
-    if err != nil {
-      log.Fatal()
-    }
-    ip, _, err := net.ParseCIDR(addr)
-    if err != nil {
-      log.Printf("failed to parse cidr: %s\n", addr)
-    }
-    if ipnet.Contains(ip) {
-      return true
-    }
-  }
-  return false
+func (a AddressChecker) CheckIfOk(addr string) bool {
+	for _, d := range a.allowlist {
+		_, ipnet, err := net.ParseCIDR(d)
+		if err != nil {
+			log.Fatal()
+		}
+		ip, _, err := net.ParseCIDR(addr)
+		if err != nil {
+			log.Printf("failed to parse cidr: %s\n", addr)
+		}
+		if ipnet.Contains(ip) {
+			return true
+		}
+	}
+	return false
 }
 
 type allowList struct {
 	Network []string `yaml:"network"`
 }
 
-func NewAddressChecker() (*AddressChecker, error){
-  t := allowList{}
-  err := yaml.Unmarshal(AllowListYaml, &t)
+func NewAddressChecker() (*AddressChecker, error) {
+	t := allowList{}
+	err := yaml.Unmarshal(AllowListYaml, &t)
 	if err != nil {
 		return nil, err
 	}
-  ret := AddressChecker{}
-  for _, n := range t.Network{
-    _, _, err := net.ParseCIDR(n)
-    if err != nil {
-      log.Fatalln(err)
-    }
-    ret.allowlist = append(ret.allowlist, n)
-  }
+	ret := AddressChecker{}
+	for _, n := range t.Network {
+		_, _, err := net.ParseCIDR(n)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		ret.allowlist = append(ret.allowlist, n)
+	}
 	return &ret, nil
 }
