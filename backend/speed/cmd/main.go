d5185a2681ec75ace267f4110c684a93874c0237
gotti <gotti@mail.gotti.dev>
2021-11-20 12:50:53 +0900

    metrics

--- backend/speed/cmd/main.go
+++ backend/speed/cmd/main.go
@@ -1,11 +1,41 @@
 package main
 
 import (
+	"github.com/prometheus/client_golang/prometheus"
 	"ueckoken/plarail2021-soft-speed/internal"
 )
 
+const namespace = "plarail2021_speed"
+
 func main() {
+	clientConn := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "clients_connections_seconds",
+			Help:      "Number of connections handling websocket",
+		},
+		[]string{},
+	)
+
+	controlCommandTotal := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "client_commands_total",
+			Help:      "Total client commands",
+		},
+		[]string{},
+	)
+
+	speed := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "speed",
+			Help:      "speed",
+		},
+		[]string{},
+	)
+
 	handler := internal.ClientHandler{Clients: &internal.ClientSet{}}
-	server := internal.SpeedServer{ClientHandler: &handler}
+	server := internal.SpeedServer{ClientHandler: &handler, NumberOfClientConnection: clientConn, TotalCLientCommands: controlCommandTotal, Speed: speed}
 	server.StartSpeed()
 }
