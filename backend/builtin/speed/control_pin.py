6bd934e4f35306ddddc0a136db67bb020c904980
Azuki-bar <42642269+Azuki-bar@users.noreply.github.com>
2021-11-20 15:14:48 +0900

    seikou control

--- backend/builtin/speed/control_pin.py
+++ backend/builtin/speed/control_pin.py
@@ -12,7 +12,7 @@ class ControlPin(http.server.BaseHTTPRequestHandler):
             self.send_response(http.HTTPStatus.BAD_REQUEST)
             return
 
-        if not 0 <= float(query['speed'][0]) <= 100:
+        if 0 <= float(query['speed'][0]) <= 100:
             change_speed(float(query["speed"][0]))
         self.send_response(http.HTTPStatus.OK)
 
@@ -20,15 +20,14 @@ class ControlPin(http.server.BaseHTTPRequestHandler):
 def setup_gpio():
     pin_channel = 10
     frequency_hz = 50
-    
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(pin_channel, GPIO.out)
+    GPIO.setup(pin_channel, GPIO.OUT)
     return GPIO.PWM(pin_channel, frequency_hz)
 
 
 def change_speed(speed: float):
     global pwm
-    pwm.changeDutyCycle(speed)
+    pwm.ChangeDutyCycle(speed)
 
 
 # start HTTP server
